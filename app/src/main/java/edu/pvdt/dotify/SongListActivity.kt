cf3ba118c428e9462d10bab507ccb21ba7b3bf49
Paola Vanegas <pvdt@uw.edu>
2021-04-15 13:46:30 -0700

    build song adapter, populate recycler view with provided song info

--- app/src/main/java/edu/pvdt/dotify/SongListActivity.kt
+++ app/src/main/java/edu/pvdt/dotify/SongListActivity.kt
@@ -2,12 +2,25 @@ package edu.pvdt.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ericchee.songdataprovider.SongDataProvider
+import edu.pvdt.dotify.databinding.ActivitySongListBinding
 
 class SongListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySongListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_list)
-
         title = "All Songs"
+        binding = ActivitySongListBinding.inflate(layoutInflater).apply{setContentView(root)}
+
+        with(binding) {
+            val songs = SongDataProvider.getAllSongs()
+
+            val adapter = SongListAdapter(songs)
+            rvSongs.adapter = adapter
+
+            // handle listeners here...
+        }
     }
 }
---
+++ app/src/main/java/edu/pvdt/dotify/SongListAdapter.kt
@@ -0,0 +1,27 @@
+package edu.pvdt.dotify
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ericchee.songdataprovider.Song
+import edu.pvdt.dotify.databinding.ItemSongBinding
+
+class SongListAdapter(private var listOfSongs: List<Song>): RecyclerView.Adapter<SongListAdapter.SongViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
+        val binding = ItemSongBinding.inflate(LayoutInflater.from(parent.context))
+        return SongViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = listOfSongs[position]
+        with(holder.binding) {
+            ivItemSongCover.setImageResource(song.smallImageID)
+            tvItemSongTitle.text = song.title
+            tvItemSongArtist.text = song.artist
+        }
+    }
+
+    override fun getItemCount(): Int = listOfSongs.size
+
+    class SongViewHolder(val binding: ItemSongBinding): RecyclerView.ViewHolder(binding.root)
+}

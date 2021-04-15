cab735c2891397d4e64986b718ade5d44ed30917
Paola Vanegas <pvdt@uw.edu>
2021-04-15 13:24:54 -0700

    add title

--- app/src/main/java/edu/pvdt/dotify/SongListActivity.kt
+++ app/src/main/java/edu/pvdt/dotify/SongListActivity.kt
@@ -7,5 +7,7 @@ class SongListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_list)
+
+        title = "All Songs"
     }
 }

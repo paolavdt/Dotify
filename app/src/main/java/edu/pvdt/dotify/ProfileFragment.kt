b8a775abd67814c7b30ae8b79d761371c904ad7b
Paola Vanegas <pvdt@uw.edu>
2021-05-10 14:01:22 -0700

    add error handling for failed requests

--- app/src/main/java/edu/pvdt/dotify/ProfileFragment.kt
+++ app/src/main/java/edu/pvdt/dotify/ProfileFragment.kt
@@ -2,6 +2,7 @@ package edu.pvdt.dotify
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,11 +14,13 @@ import edu.pvdt.dotify.databinding.FragmentProfileBinding
 import edu.pvdt.dotify.databinding.FragmentStatisticsBinding
 import edu.pvdt.dotify.repository.DataRepository
 import kotlinx.coroutines.launch
+import java.lang.Error
+import java.lang.Exception
 
 class ProfileFragment : Fragment() {
 
     lateinit var dotifyApp: DotifyApplication
-    private lateinit var dataRepository: DataRepository
+    lateinit var dataRepository: DataRepository
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -31,13 +34,19 @@ class ProfileFragment : Fragment() {
     ): View? {
         val binding = FragmentProfileBinding.inflate(layoutInflater)
         with(binding) {
-            lifecycleScope.launch{
-                val user = dataRepository.getUser()
-                ivProfileImage.load(user.profilePicURL)
-                tvProfileUsername.text = user.username
-                tvFirstName.text = getString(R.string.profile_desc, "First Name", user.firstName)
-                tvLastName.text = getString(R.string.profile_desc, "Last Name", user.lastName)
-                tvHasNose.text = getString(R.string.profile_desc, "Has Nose", user.hasNose.toString())
+            lifecycleScope.launch {
+                try {
+                    val user = dataRepository.getUser()
+                    ivProfileImage.load(user.profilePicURL)
+                    tvProfileUsername.text = user.username
+                    tvFirstName.text = getString(R.string.profile_desc, "First Name", user.firstName)
+                    tvLastName.text = getString(R.string.profile_desc, "Last Name", user.lastName)
+                    tvHasNose.text = getString(R.string.profile_desc, "Has Nose", user.hasNose.toString())
+                    tvError.visibility = View.GONE
+                } catch (exception: Exception) {
+                    tvError.visibility = View.VISIBLE
+                    Toast.makeText(activity, exception.toString(), Toast.LENGTH_LONG).show()
+                }
             }
         }
         return binding.root
